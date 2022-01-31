aa20f9a4cfd7f1ea174078e03557e04a538f176d
Pushpendra <pkanaujiya91@gmail.com>
2022-01-31 21:00:11 +0530

    app created

--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello")
+    return render(request, "index.html")
 
 def signup(request):
     return render(request, "signup.html")
