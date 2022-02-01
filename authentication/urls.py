d285abb707d634ae054c61a4f054c4ff7ea59c8b
Pushpendra <pkanaujiya91@gmail.com>
2022-02-02 02:14:07 +0530

    created form

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
        path('', views.home, name="home"),
        path('signup', views.signup, name='signup'),
        path('signin', views.signin, name='signin'),
-       path('signout', views.signout, name='signin'),
+       path('signout', views.signout, name='signout'),
 
 ]
