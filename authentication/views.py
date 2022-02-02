f3fc48ac6d8ab7c365f5cf0074b1d86f829df5db
Pushpendra <pkanaujiya91@gmail.com>
2022-02-02 20:10:39 +0530

    form updated

--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(request):
     return render(request, "authentication/index.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        name = request.POST['name']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.name = name
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('signin')
+
+
     return render(request, "authentication/signup.html")
 
 def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            name = user.username
+            return render(request, "authentication/index.html", {'name':name})
+        
+        else:
+            messages.error(request, "Invalid User")
+            return redirect('home')
+
     return render(request, "authentication/signin.html")
 
 def signout(request):
